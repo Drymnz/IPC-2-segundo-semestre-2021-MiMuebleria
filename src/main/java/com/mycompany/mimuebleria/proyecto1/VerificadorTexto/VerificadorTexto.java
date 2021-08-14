4c75b1c4b81b22246d883710cf4eb27e199d1683
Benjamín de Jesús Pérez Aguilar <benjaminperez201831504@cunoc.edu.gt>
2021-08-14 09:27:35 -0600

    fue terminado la clase verificar

--- src/main/java/com/mycompany/mimuebleria/proyecto1/VerificadorTexto/VerificadorTexto.java
+++ src/main/java/com/mycompany/mimuebleria/proyecto1/VerificadorTexto/VerificadorTexto.java
@@ -5,10 +5,34 @@
  */
 package com.mycompany.mimuebleria.proyecto1.VerificadorTexto;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author drymnz
  */
 public class VerificadorTexto {
-    
+
+    private String numero = "\\d";
+
+    //constructor
+    public VerificadorTexto() {
+    }
+    //fin constructor
+    // verificadores
+    public boolean verificarEsNumero(String verifcar){
+        if (verificador(numero, verifcar)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    // fin verificadores
+    // usando 
+    public boolean verificador(String parametro, String verificar){
+        Pattern objetoparametro = Pattern.compile(parametro);
+        Matcher analizador = objetoparametro.matcher(verificar);
+        return analizador.matches();
+    }
 }
